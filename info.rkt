e0778fa5186ebbae3d0f1d3e833a9017f9e1125b
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-11-26 16:40:33 -0500

    Add info.rkt.

---
+++ info.rkt
@@ -0,0 +1,5 @@
+#lang info
+(define collection "pkg-build")
+(define deps '("base" "rackunit" "scribble-html-lib" "web-server-lib"
+               "plt-web-lib" "remote-shell-lib"))
+(define build-deps '("at-exp-lib"))
