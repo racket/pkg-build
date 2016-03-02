5042ece558fd2b963a15a5869b1c59470a6e44d0
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-03-02 10:21:33 -0500

    Add explicit dependency on v1.1 for `get-vbox-snapshot-uuid`.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection "pkg-build")
 (define deps '("base" "rackunit" "scribble-html-lib" "web-server-lib"
-               "plt-web-lib" "remote-shell-lib"))
+               "plt-web-lib" ("remote-shell-lib" #:version "1.1")))
 (define build-deps '("at-exp-lib"))
